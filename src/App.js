8494fe4e3eae6078b6403eb0eb5ddef7d3327483
Kaue-LS <kaue.ls0506@gmail.com>
2023-01-22 20:35:25 -0300

    add files carousel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
-
+import superShoes from './components/static/images/super-shoes.png'
+import shoesData from './components/static/shoes.json'
+import setas from './components/static/images/seta.png'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [data, setData] = useState([])
+  const carouselRef = useRef(null)
+  useEffect(() => {
+    setData(shoesData)
+  }, [])
+
+  const HandleLeftClick = (e) => {
+    e.preventDefault()
+    carouselRef.current.scrollLeft -= carouselRef.current.offsetWidth
+    console.log("left",carouselRef.current.scrollLeft)
+  }
+  const HandleRightClick = (e) => {
+    e.preventDefault()
+    carouselRef.current.scrollLeft += carouselRef.current.offsetWidth
+    console.log("right",carouselRef.current.scrollLeft)
+  }
+    console.log(carouselRef)
+  return (data || data.length > 0) && (
+    <div className="container">
+      <div className='logo'>
+        <img src={superShoes} alt='Super logo' />
+      </div>
+      <div className='carousel' ref={carouselRef}>
+        {data.map((item) => (
+          <div className='item' key={item.id}>
+            <div className='image'>
+              <img src={item.image} alt={item.id} />
+            </div>
+
+            <div className='info'>
+              <span className='name'>{item.name}</span>
+              <span className='oldPrice'>{item.oldPrice}</span>
+              <span className='price'>{item.price}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div style={{maxWidth:"718px", background:"Gray"}}>
+          teste
+      </div>
+      <div className='buttons'>
+        <button onClick={HandleLeftClick}><img src={setas} alt="Scroll Left" /></button>
+        <button onClick={HandleRightClick}><img src={setas} alt="Scroll Right" /></button>
+
+      </div>
     </div>
   );
 }
